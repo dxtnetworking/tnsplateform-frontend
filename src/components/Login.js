47839a1622bf909319a3e173c3c1fd314ee6afdf
Demba Siby <dembasiby@gmail.com>
2021-02-08 12:05:26 +0000

    Mise à jour UI du formulaire

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Button, Form, Input, Grid, Header as SUHeader, Image, Message, Segment } from 'semantic-ui-react'
 
 class Login extends React.Component {
   gestionConnexion = (e) => {
@@ -18,44 +18,28 @@ class Login extends React.Component {
   };
   render() {
     return (
-      <div>      
         <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-          <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2' color='teal' textAlign='center'>
-            <Image src='/logo.png' /> Log-in to your account
-          </Header>
-          <Form size='large' onSubmit={this.gestionConnexion}>
+          <Grid.Column  style={{ maxWidth: 450 }}>
+            <SUHeader as='h2' textAlign='center'>
+              Se connecter!
+            </SUHeader>
             <Segment stacked>
-              <Form.Input fluid 
-                icon='user' 
-                iconPosition='left' 
-                placeholder='Adresse e-mail' 
-                type="email"
-                name="email"
-                ref={(input) => (this.email = input)}
-              />
-              <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Mot de passe'
-                type='password'
-                name="password"
-                ref={(input) => (this.password = input)}
-              />
-
-              <Button type='submit' color='teal' fluid size='large'>
-                Se connecter
-              </Button>
+              <form className="ui form" onSubmit={this.gestionConnexion} >
+                <div className="fluid field">
+                  <label for="email" className="">Email</label>
+                  <input type="email" name="email" ref={(input) => this.email = input}/>
+                </div>
+                <div className="fluid field">
+                  <label for="password" className="">Mot de passe</label>
+                  <input type="password" name="password" ref={(input) => this.password = input}/>
+                </div>
+                <div>
+                  <Button type="submit" color="primary" size="large">Se connecter</Button>
+                </div>
+              </form>
             </Segment>
-          </Form>
-          <Message>
-            Pas encore inscrit? <a href='#'>S'inscrire</a>
-          </Message>
-        </Grid.Column>
+          </Grid.Column>
         </Grid>
-      </div>
-
     );
   }
 }
